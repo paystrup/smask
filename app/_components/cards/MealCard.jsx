b9b56606dcc4340ec124042be1800d3e87526f0e
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2024-10-28 01:22:49 +0100

    Add a basic meal card component

--- app/_components/cards/MealCard.jsx
+++ app/_components/cards/MealCard.jsx
@@ -11,21 +11,26 @@ export default function MealCard({
   imageUrl,
   link,
 }) {
+  const tagsToDisplay = 2;
+
   return (
     <Link to={link}>
-      <div className="rounded-2xl shadow-md w-full h-fit">
+      <div className="rounded-2xl shadow-sm w-full h-fit relative text-white">
         <div className="relative">
           {imageUrl ? (
             <img
               src={imageUrl}
               alt={title}
-              className="w-full h-72 object-cover rounded-t-2xl"
+              className="w-full h-80 object-cover rounded-2xl"
             />
           ) : (
-            <div className="flex items-center justify-center text-3xl bg-slate-50 w-full h-72 object-cover rounded-t-2xl">
+            <div className="flex items-center justify-center text-3xl bg-slate-50 w-full h-80 object-cover rounded-2xl">
               <p>🍔</p>
             </div>
           )}
+
+          {/* Gradient overlay */}
+          <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-2xl" />
 
           {seasons.length > 0 && (
             <div className="absolute top-0 left-0 p-4">
@@ -36,25 +41,37 @@ export default function MealCard({
           )}
         </div>
 
-        <div className="p-6 rounded-b-2xl">
-          <h3 className="text-2xl font-bold">{title}</h3>
-          <p>{description}</p>
+        <div className="p-6 rounded-b-2xl flex flex-col gap-2 absolute bottom-0 left-0">
+          <div>
+            <h3 className="text-2xl font-bold first-letter:capitalize">
+              {title}
+            </h3>
+            <p className="opacity-80 first-letter:capitalize line-clamp-1">
+              {description}
+            </p>
+          </div>
 
-          {tags.length > 0 && (
-            <div>
-              {tags.map((tag) => (
-                <Badge key={tag}>{tag.name}</Badge>
-              ))}
-            </div>
-          )}
+          <ul className="flex flex-wrap gap-2">
+            {tags.length > 0 && (
+              <>
+                {tags.slice(0, tagsToDisplay).map((tag) => (
+                  <li key={tag} className="list-none">
+                    <Badge>{tag.name}</Badge>
+                  </li>
+                ))}
+              </>
+            )}
 
-          {allergies.length > 0 && (
-            <div>
-              {allergies.map((allergy) => (
-                <Badge key={allergy}>{allergy}</Badge>
-              ))}
-            </div>
-          )}
+            {allergies.length > 0 && (
+              <>
+                {allergies.slice(0, tagsToDisplay).map((allergy, i) => (
+                  <li key={allergy + i} className="list-none">
+                    <Badge>{allergy}</Badge>
+                  </li>
+                ))}
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </Link>

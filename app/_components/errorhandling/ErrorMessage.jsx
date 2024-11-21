a09f23716fcbb5d9c674a0355c03edccc7ffa833
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2024-11-21 05:34:03 +0100

    Adjust error message component font styling

--- app/_components/errorhandling/ErrorMessage.jsx
+++ app/_components/errorhandling/ErrorMessage.jsx
@@ -2,13 +2,13 @@ export default function ErrorMessage({ title, message, className }) {
   return (
     <div
       className={[
-        "rounded border border-red-500 bg-red-100 p-4 text-red-900",
+        "rounded border border-red-500 bg-red-100 p-4 text-red-900 m-12",
         className,
       ]
         .filter(Boolean)
         .join(" ")}
     >
-      <h2 className="mb-3 text-xl font-bold">{title}</h2>
+      <h2 className="mb-3 text-lg font-bold">{title}</h2>
       <p>{message}</p>
     </div>
   );

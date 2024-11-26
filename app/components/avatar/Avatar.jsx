fda129ff9f680b56fda64ed52f6dfae50ac667f3
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2024-11-26 06:28:06 +0100

    Add cn to avatar, to allow classname merges w tw

--- app/components/avatar/Avatar.jsx
+++ app/components/avatar/Avatar.jsx
@@ -1,7 +1,14 @@
+import { cn } from "~/lib/utils";
+
 /* eslint-disable react/prop-types */
-export default function Avatar({ name }) {
+export default function Avatar({ name, className }) {
   return (
-    <div className="h-8 w-8 font-semibold text-md rounded-full flex items-center justify-center bg-black text-white leading-none">
+    <div
+      className={cn(
+        "h-8 w-8 font-semibold text-md rounded-full flex items-center justify-center bg-black text-white leading-none",
+        className,
+      )}
+    >
       <p>{name ? name?.charAt(0).toUpperCase() : "?"}</p>
     </div>
   );

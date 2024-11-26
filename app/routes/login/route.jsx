0c79d0118657dfbeddadfef66dfcec8e206988fc
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2024-11-26 06:29:37 +0100

    Use card in login route

--- app/routes/login/route.jsx
+++ app/routes/login/route.jsx
@@ -11,6 +11,13 @@ import { sessionStorage } from "~/services/session.server";
 import ErrorMessage from "~/components/errorhandling/ErrorMessage";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export function meta() {
   return [{ title: "smask | Login" }];
@@ -36,68 +43,71 @@ export default function LoginPage() {
   const loaderData = useLoaderData();
 
   return (
-    <section className="flex flex-col items-center px-4 lg:px-8 2xl:px-0">
-      <div className="flex flex-col items-center gap-4 max-w-xl w-full">
-        <div className="flex flex-col gap-6 my-12 text-center max-w-[45ch]">
-          <h1 className="text-5xl font-semibold tracking-tighter">
+    <section className="flex flex-col items-center lg:px-8 2xl:px-0 py-10">
+      <Card className="max-w-lg w-full p-4">
+        <CardHeader className="flex flex-col items-center">
+          <CardTitle className="text-4xl tracking-tighter text-center">
             Login to use Smask
-          </h1>
-          <p className="text-xl">
-            You must login to access all features of Smask
-          </p>
-        </div>
-        <Form method="post" className="flex flex-col w-full gap-8">
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <label htmlFor="email">Email</label>
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Your email"
-                required
-              />
+          </CardTitle>
+          <CardDescription className="text-center max-w-[30ch]">
+            Login with your email and password below to access all features of
+            Smask
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent className="text-sm">
+          <Form method="post" className="flex flex-col w-full gap-8 mb-2">
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-1">
+                <label htmlFor="email">Email</label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Your email"
+                  required
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <div className="flex justify-between">
+                  <label htmlFor="password">Password</label>
+                  <Link
+                    to="/forgotpassword"
+                    className="hover:opacity-60 duration-200 transition-all"
+                  >
+                    <u>Forgot password?</u>
+                  </Link>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Your password"
+                  autoComplete="current-password"
+                  required
+                />
+              </div>
             </div>
 
-            <div className="flex flex-col gap-1">
-              <label htmlFor="password">Password</label>
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Your password"
-                autoComplete="current-password"
-                required
-              />
-            </div>
-          </div>
+            {/* SHOW LOGIN ERR */}
+            {loaderData?.error?.message && (
+              <div className="text-red-500">
+                <p>{loaderData?.error?.message}</p>
+              </div>
+            )}
 
-          {/* SHOW LOGIN ERR */}
-          {loaderData?.error?.message && (
-            <div className="text-red-500">
-              <p>{loaderData?.error?.message}</p>
-            </div>
-          )}
+            <Button type="submit">Sign In</Button>
+          </Form>
 
-          <Button type="submit">Sign In</Button>
-        </Form>
-
-        <div className="flex flex-col text-center sm:flex-row sm:text-start justify-between gap-6 sm:gap-2 w-full">
           <Link
             to="/signup/location"
-            className="hover:opacity-60 duration-200 transition-all"
+            className="hover:opacity-60 duration-200 transition-all "
           >
-            Don&apos;t have a user? Sign up here 🎉
+            Don&apos;t have a user? <u>Sign up here</u>
           </Link>
-
-          <Link
-            to="/forgotpassword"
-            className="hover:opacity-60 duration-200 transition-all"
-          >
-            Forgot password
-          </Link>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </section>
   );
 }

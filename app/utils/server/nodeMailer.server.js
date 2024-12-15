1a36648531224cbb7ae35fc3e097ecc08486bdae
Nikolaj Pejstrup <nikolajpaystrup@gmail.com>
2024-12-16 00:48:44 +0100

    Update resend mail to smask.app

--- app/utils/server/nodeMailer.server.js
+++ app/utils/server/nodeMailer.server.js
@@ -18,7 +18,7 @@ export const sendintroMail = async (email, name) => {
 
   try {
     await resend.emails.send({
-      from: `SMASK <smask@resend.dev>`,
+      from: `SMASK <smask@smask.app>`,
       to: email,
       subject: "Welcome to SMASK",
       html: `<p>Hi ${name},</p><p>Thank you for signing up. Welcome to smask! We are excited to have you on board. Go to our login page to get started. </p><a href="${domain}/login">Go to login</a>`,
@@ -36,7 +36,7 @@ export const sendDeletionMail = async (email, name) => {
 
   try {
     await resend.emails.send({
-      from: `SMASK <smask@resend.dev>`,
+      from: `SMASK <smask@smask.app>`,
       to: email,
       subject: `We are going to miss you ${name}`,
       html: `<p>Hi ${name},</p><p>Sorry to let you go. We have deleted your account and all your information from our service. It's been a great ride, hope to see you back soon!</p>`,
@@ -56,7 +56,7 @@ export const sendResetLink = async (email, name, resetLink) => {
 
   try {
     await resend.emails.send({
-      from: `SMASK <smask@resend.dev>`,
+      from: `SMASK <smask@smask.app>`,
       to: email,
       subject: "Password reset request - Smask",
       html: `
@@ -86,7 +86,7 @@ export const sendConfirmPasswordReset = async (email, name) => {
 
   try {
     await resend.emails.send({
-      from: `SMASK <smask@resend.dev>`,
+      from: `SMASK <smask@smask.app>`,
       to: email,
       subject: "Your password has been reset",
       html: `<p>Hi ${name},</p><p>Your password has been reset. If you did not perform this action, please freeze your account immediately. Login with your new password here.</p><a href="${domain}/login">Go to login</a>`,
@@ -111,7 +111,7 @@ export const sendChangedPasswordConfirmation = async (email, name) => {
 
   try {
     await resend.emails.send({
-      from: `SMASK <smask@resend.dev>`,
+      from: `SMASK <smask@smask.app>`,
       to: email,
       subject: "Your password has been changed",
       html: `<p>Hi ${name},</p><p>Your password has been changed. If you did not perform this action, please freeze your account immediately. Login with your new password here.</p><a href="${domain}/login">Go to login</a>`,
@@ -138,7 +138,7 @@ export const sendReminder = async (email, name, days) => {
 
   try {
     await resend.emails.send({
-      from: "SMASK <smask@resend.dev>",
+      from: "SMASK <smask@smask.app>",
       to: email,
       subject: "Missing attendance next week",
       html: `
